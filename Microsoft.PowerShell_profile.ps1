758f8741de3745038918ca76e6d1c206071a508d
Camillus Gerard Cai <cgcai@qxcg.net>
2014-06-01 19:10:05 -0700

    Initial commit.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Replicate OS X `open` command.
+Set-Alias open Invoke-Item
+
+# `subl` launches Sublime Text 3.
+Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
+
+# `which` -> `where.exe`
+Set-Alias which where.exe
